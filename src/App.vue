c841954d51b22a7525c39e60f51cc15cbe57bb7f
Jean Martins <jean.martins0000@gmail.com>
2018-12-12 20:43:38 -0200

    teste

--- src/App.vue
+++ src/App.vue
@@ -65,7 +65,7 @@ export default {
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Lembrete de Leitura de Livros'
+      title: 'Lembrete de Leitura'
     }
   },
   name: 'App'
